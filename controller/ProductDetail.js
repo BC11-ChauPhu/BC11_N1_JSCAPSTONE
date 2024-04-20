63ea5abe212edd945383c3fa474c5abc1debc035
Phu <chauphu159951@gmail.com>
2024-04-20 16:01:36 +0700

    them cart vao product detail

--- controller/ProductDetail.js
+++ controller/ProductDetail.js
@@ -1,17 +1,52 @@
-// let getElement = (selector) => {
-//     return document.querySelector(selector);
-// }
+let products = []
+
+let pushProducts = (data) => {
+    products.push(...data)
+}
+
 
 let getProduct = () => {
     let objAxios = axios({
         method: 'get',
         url: 'https://shop.cyberlearn.vn/api/Product'
     })
-    objAxios.then(function(result){
-        console.log(result)
-    }).catch(function(error){
+    objAxios.then(function (result) {
+        showProductFeatures(result.data.content)
+        pushProducts(result.data.content)
+    }).catch(function (error) {
         console.log(error)
     })
 }
 
 getProduct()
+
+let showProductFeatures = (products) => {
+    let content = ''
+    products.slice(8, 11).map(function (product) {
+        content += `
+        <div class="item">
+            <div class="item-inner">
+                <div class="item-img">
+                    <a href="#">
+                        <img src=${product.image}>
+                    </a>
+                </div>
+                <div class="item-body">
+                    <h3>${product.name}</h3>
+                    <p>${product.description}</p>
+                    <div class="price mt-2">
+                        <span><i class="fa-solid fa-tag" aria-hidden="true"></i>$${product.price}</span>
+                        <button class="buyButtonInverted" type="button" data-id="${product.id}">
+                            <i class="fa-solid fa-cart-plus" aria-hidden="true"></i>
+                            Buy Now
+                        </button>
+                    </div>
+                </div>
+        </div>
+    </div>
+        `
+    })
+
+    getElement('#catalouge-item').innerHTML = content
+}
+
